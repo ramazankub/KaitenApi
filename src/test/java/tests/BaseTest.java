30d70750621026b08523395d2f7af31b08f434e2
user <user@users-MacBook-Pro.local>
2026-02-15 03:01:37 +0500

    Made Confireader

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,23 +3,23 @@ package tests;
 import constants.Constant;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import readers.ConfigReader;
 
 import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.delete;
 
 public class BaseTest {
 
     public Constant constant = new Constant();
-    public String token = "Bearer 48578141-5fad-41e3-a3f3-15424454e61d";
+    public String token = ConfigReader.getToken();
 
     @BeforeTest
-public void setUp() {
-    baseURI = "https://rmznkubanov.kaiten.ru/api/latest/";
-}
+    public void setUp() {
+        baseURI = ConfigReader.getBaseURI();
+    }
 
 
     @AfterTest
     public void tearDown() {
-
+        System.out.println("Тесты окончены");
     }
 }
